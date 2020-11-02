7b293670c86740eb01f18c952060c0582e223cfa
Tony Gao <gwjlshebei@gmail.com>
2020-11-02 23:16:52 +0800

    更新装饰模式

--- 装饰器模式/1.ts
+++ 装饰器模式/1.ts
@@ -9,65 +9,113 @@
 
 // 实现思想：抛弃继承，使用聚合/组合的方式来实现效果，即装饰器模式
 
+// 装饰模式是一种结构型设计模式， 允许你通过将对象放入包含行为的特殊封装对象中来为原对象绑定新的行为。
+
 // 实现步骤：
 
-// 确保业务逻辑可用一个基本组件及多个额外可选层次表示。
+// 1. 确保业务逻辑可用一个基本组件及多个额外可选层次表示。
 
-// 找出基本组件和可选层次的通用方法。 创建一个组件接口并在其中声明这些方法。
+// 2. 找出基本组件和可选层次的通用方法。 创建一个组件接口并在其中声明这些方法。
 
-// 创建一个具体组件类， 并定义其基础行为。
-
-// 创建装饰基类， 使用一个成员变量存储指向被封装对象的引用。 该成员变量必须被声明为组件接口类型， 从而能在运行时连接具体组件和装饰。 装饰基类必须将所有工作委派给被封装的对象。
-
-// 确保所有类实现组件接口。
-
-// 将装饰基类扩展为具体装饰。 具体装饰必须在调用父类方法 （总是委派给被封装对象） 之前或之后执行自身的行为。
-
-// 客户端代码负责创建装饰并将其组合成客户端所需的形式。
-
-// 1. 抽象行为接口
-interface Action {
-  eat(): void;
-}
-// 2. 基础实现类
-class Person implements Action {
-  eat() {
-    console.log("基础类：人吃东西");
-  }
-}
-// 3. 修饰类, 实现共同接口
-class Decorator implements Action {
-  constructor(private p: Action) {}
-  eat() {
-    console.log("装饰类:吃之前的动作");
-    this.p.eat();
-    console.log("装饰类:吃之之后的动作");
-  }
-}
-// 4. 增强修饰类
-class EnhancedDecorator extends Decorator {
-  eat() {
-    console.log("增强修饰类前");
-    super.eat();
-    console.log("增强修饰类后");
+namespace step2 {
+  interface Component {
+    operation(): void;
   }
 }
 
-// 客户端代码
-function clientCode(p: Action) {
-  p.eat();
-  /* 代码修改区域 */
-  //   不对源代码修改，给Person增强
-  p = new Decorator(p);
-  console.log("================================");
-  p.eat();
-  //   超级增强
-  p = new EnhancedDecorator(p);
-  console.log("=================================");
-  p.eat();
-  /* 代码修改区域 */
+// 3. 创建一个具体组件类， 并定义其基础行为。
+namespace step3 {
+  interface Component {
+    operation(): void;
+  }
+  class BaseComponent implements Component {
+    constructor(private name: string) {}
+    operation() {
+      console.log(`${this.name} is doing basic operation`);
+    }
+  }
 }
-let p = new Person();
-clientCode(p);
+
+// 4. 创建装饰基类， 使用一个成员变量存储指向被封装对象的引用。
+// 该成员变量必须被声明为组件接口类型， 从而能在运行时连接具体组件和装饰。
+// 装饰基类必须将所有工作委派给被封装的对象。
+
+namespace step4 {
+  interface Component {
+    operation(): void;
+  }
+  class BaseComponent implements Component {
+    constructor(private name: string) {}
+    operation() {
+      console.log(`${this.name} is doing basic operation`);
+    }
+  }
+  class Decorator {
+    constructor(private wrappedComp: BaseComponent) {}
+  }
+}
+
+// 5. 确保所有类实现组件接口。
+namespace step5 {
+  interface Component {
+    operation(): void;
+  }
+  class BaseComponent implements Component {
+    constructor(private name: string) {}
+    operation() {
+      console.log(`${this.name} is doing basic operation`);
+    }
+  }
+  class Decorator {
+    constructor(private wrappedComp: BaseComponent) {}
+    operation() {
+      this.wrappedComp.operation();
+    }
+  }
+}
+
+// 6. 将装饰基类扩展为具体装饰。 具体装饰必须在调用父类方法 （总是委派给被封装对象） 之前或之后执行自身的行为。
+// 在实际使用的时候，也可能不必抽象出基础装饰类和具体装饰类，直接在基础装饰类上就可以进行修改。
+namespace step6 {
+  interface Component {
+    operation(): void;
+  }
+  class BaseComponent implements Component {
+    constructor(public name: string) {}
+    operation() {
+      console.log(`${this.name} is doing basic operation`);
+    }
+  }
+  class Decorator {
+    constructor(public name: string, protected wrappedComp: BaseComponent) {}
+    operation() {
+      console.log(`${this.name} running base decorator operation`);
+      this.wrappedComp.operation();
+    }
+  }
+  class ConcreteDecorator extends Decorator {
+    operation() {
+      console.log(`${this.name} running concrete decorator operation`);
+      super.operation();
+    }
+  }
+
+  // 客户端代码负责创建装饰并将其组合成客户端所需的形式。
+  function clientCode(arg: Component) {
+    arg.operation();
+  }
+
+  // 基础组件执行operation
+  let simple = new BaseComponent("basic compo");
+  clientCode(simple);
+  console.log("=============================");
+
+  simple = new Decorator("decorated compo", simple);
+  clientCode(simple);
+  console.log("=============================");
+
+  simple = new ConcreteDecorator("concrete decorated compo", simple);
+  clientCode(simple);
+}
 
 export {};
